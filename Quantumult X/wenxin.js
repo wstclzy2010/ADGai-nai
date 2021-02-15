2f14264a1a939f07bbe39a8f61b59ea211692dd6
AD <59598230+Zhuliyer@users.noreply.github.com>
2021-02-15 12:48:10 +0800

    Update wenxin.js

--- Quantumult X/wenxin.js
+++ Quantumult X/wenxin.js
@@ -1,29 +1,11 @@
+str = str.match(/:&#x2f;&#x2f;(\S*)"}/)[1]
+str = str.replace(/&#x2f;/g, '/');
+str = str.replace(/&amp;/g, '&');
+console.log(str);//结果bbbcccdddeee 
 
-
-var str = ($response.body);
-
-str = str.match(/:&#x2f;&#x2f;(\S*)"}/)[1].replace(/&#x2f;/g, '/').replace(/&amp;/g, '&').split("\"")[0]
-let opener = str.indexOf("m.tb.cn") != -1 ? "taobao://" + str: ($response.body)
-let opener = str.indexOf("*.com") != -1 ? "*.com://" + str: ($response.body)
-//console.log(str);
-
-const $ = new cmp()
-
-if (str.indexOf("m.tb.cn","*.com") != -1) {
-    $.notify(``, "", "😂点击打开", opener)
-} else if (str.indexOf("如需浏览")) {
-    $.notify(``,"", "🔗点击打开链接", "https://"+str)
-}
+var option1={"open-url": "*.com://"}
+	option1["open-url"]="*.com://"+str
+	$notify(``, "","🔗点击打开链接", option1);
+console.log(option1["open-url"])
 
 $done({body: $response.body});
-
-function cmp() {
-    _isQuanX = typeof $task != "undefined"
-    _isLoon = typeof $loon != "undefined"
-    _isSurge = typeof $httpClient != "undefined" && !_isLoon
-    this.notify = (title, subtitle, message, url) => {
-        if (_isLoon) $notification.post(title, subtitle, message, url)
-        if (_isQuanX) $notify(title, subtitle, message, { "open-url": url })
-        if (_isSurge) $notification.post(title, subtitle, message, { url: url })
-    }
-}
